5cd048cc04331476a6f36f5eb634888d56cad477
Assraful <mdassrafulislam99@gmail.com>
2023-05-07 21:49:52 -0700

    update sing in

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+
+import './App.css'
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import app from './firebase/firebase.config'
+import { useState } from 'react';
+
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+function App() {
+  const [user, setUser] = useState(null);
+
+  const handleGoogleSingIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const loggedUser = result.user
+        setUser(loggedUser)
+
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
+  }
+  return (
+    <>
+      <div>
+      </div>
+      <h1>Timple + React</h1>
+      <button onClick={handleGoogleSingIn}>Google Sing In</button>
+      {user && <div className="card">
+        <h3>Email: {user.displayName}</h3>
+        <img src={user.photoURL} alt="" />
+      </div>}
+
+    </>
+  )
+}
+
+export default App
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCA1OzaNXNWb5YTNBn13TxPzFNULy9kEmg",
+  authDomain: "timple-firebase-9f35a.firebaseapp.com",
+  projectId: "timple-firebase-9f35a",
+  storageBucket: "timple-firebase-9f35a.appspot.com",
+  messagingSenderId: "760670243978",
+  appId: "1:760670243978:web:d71c15d25b623d9b9cf173"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
